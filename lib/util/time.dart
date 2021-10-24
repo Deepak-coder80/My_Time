9d3ff8de2ed0400ad80c00583f1f351402140d41
Deepak M S <souparnikad40@gmail.com>
2021-10-24 18:17:07 +0530

    countdown timer class created

--- lib/util/time.dart
+++ lib/util/time.dart
@@ -1,2 +1,11 @@
 import 'dart:async';
+// ignore: unused_import
 import 'package:my_time/model/model.dart';
+
+class CountDownTimer {
+  double _radius = 1;
+  bool _isActive = true;
+  late Timer timer;
+  late Duration _time;
+  late Duration _fullTime;
+}
